3204cb24d0cea8624aa484a6813f06c0ebd79c94
Dangggchris <christophermtdang@gmail.com>
2019-06-13 20:55:00 -0700

    get and post function for reservation and waitlist

--- taylorsteam.js
+++ taylorsteam.js
@@ -19,10 +19,25 @@ var waitlist = [];
 
 // Routes
 // =============================================================
+// 
+app.get("/api/reservation", function(req,res) {
+  res.json(reservation);
+});
 
+app.get("/api/waitlist", function(req,res) {
+  res.json(waitlist);
+});
 
-
-
+app.post("/api/reservation", function(req,res) {
+  if (reservation.length < 5) {
+    reservation.push(req.body);
+    res.json(true);
+  }
+  else {
+    waitlist.push(req.body);
+    res.json(false);
+  }
+})
 
 
 
