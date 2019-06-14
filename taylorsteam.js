0119686e6b8c9e65ebe24c3e1b98aa7dee76019a
Tomas <tslemenson@gmail.com>
2019-06-13 20:21:03 -0700

    basics

---
+++ taylorsteam.js
@@ -0,0 +1,31 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// lsSets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Routes
+// =============================================================
+
+
+
+
+
+
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
+  
