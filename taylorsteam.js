bd08df7d67986686814925d63f69631c46445d3a
Taylor Dougherty <45830707+taydougherty@users.noreply.github.com>
2019-06-13 20:28:59 -0700

    added json display

--- taylorsteam.js
+++ taylorsteam.js
@@ -19,15 +19,21 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "home.html"));
   });
   
-app.get("/api/tables", function(req, res) {
+app.get("/tables", function(req, res) {
 res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/api/reserve", function(req, res) {
+app.get("/reserve", function(req, res) {
 res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
+app.get("/api/tables", function(req, res) {
+    return res.json(reservation);
+  });
 
+app.get("/api/reserve", function(req, res) {
+    return res.json(waitlist);
+});
 
 
 
